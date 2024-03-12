81b399325820f823c6c8f0b5b35a323208de2583
peaches <peizhe2001@hotmail.com>
2024-03-11 21:31:22 -0400

    Add profile pic spot

--- src/app/(tabs)/three.tsx
+++ src/app/(tabs)/three.tsx
@@ -70,8 +70,9 @@ export default function TabTwoScreen() {
     );
   };
   return (
-    <View className="flex items-center pt-32 h-full bg-w2 dark:bg-bl">
-      <View className="flex h-1/4">
+    <View className="flex items-center pt-24 h-full bg-w2 dark:bg-bl">
+      <View className="flex h-1/4 items-center mb-24">
+        <View className="rounded-full aspect-square w-40 bg-w1 mb-10"></View>
         <Text className="text-3xl font-bold text-bl dark:text-w2">
           Profile Page
         </Text>
