ebace2f7dc72f7aae1ad2fb010ce37d7993a4d72
peaches <peizhe2001@hotmail.com>
2024-03-12 22:36:28 -0400

    Finish transitional modal for starting workout

--- src/app/Home/Workouts.tsx
+++ src/app/Home/Workouts.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   FlatList,
   SafeAreaView,
@@ -7,8 +7,12 @@ import {
   View,
   ImageBackground,
   ImageRequireSource,
+  StyleSheet,
+  Modal,
+  Alert,
 } from "react-native";
-import { Link } from "expo-router";
+import { Link, useNavigation } from "expo-router";
+import { set } from "@gluestack-style/react";
 
 type ItemData = { id: string; name: string; image: ImageRequireSource };
 
@@ -32,43 +36,89 @@ const workout: ItemData[] = [
 
 type WorkoutActivityProps = {
   item: ItemData;
+  // setModalVisible: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 const WorkoutActivity = ({ item }: WorkoutActivityProps) => {
-  // If the item is "History", we use the Link component to navigate to the history screen
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
-    <Link
-      href={{ pathname: "/session", params: { workoutName: item.name } }}
-      asChild
+    <TouchableOpacity
+      activeOpacity={0.6}
+      className="rounded-lg bg-w1 dark:bg-bl2 aspect-square w-36 mr-3 flex justify-center items-center"
+      onPress={() => setModalVisible(true)}
     >
-      <TouchableOpacity
-        activeOpacity={0.6}
-        className="rounded-lg bg-w1 dark:bg-bl2 aspect-square w-36 mr-3 flex justify-center items-center"
+      <ImageBackground
+        source={item.image} // Replace with the path to your image
+        style={{
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        imageStyle={{
+          borderRadius: 8, // Set borderRadius for ImageBackground
+          opacity: 0.8,
+        }}
+        resizeMode="cover" // or "contain" depending on your preference
       >
-        <ImageBackground
-          source={item.image} // Replace with the path to your image
-          style={{
-            width: "100%",
-            height: "100%",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-          imageStyle={{
-            borderRadius: 8, // Set borderRadius for ImageBackground
-            opacity: 0.8,
-          }}
-          resizeMode="cover" // or "contain" depending on your preference
-        >
-          <Text className="text-w1 dark:text-gr text-shadow-[0_2px_15px_#0d0d0f] font-chivo text-lg text-center">
-            {item.name}
-          </Text>
-        </ImageBackground>
-      </TouchableOpacity>
-    </Link>
+        <Text className="text-w1 dark:text-gr text-shadow-[0_2px_15px_#0d0d0f] font-chivo text-lg text-center">
+          {item.name}
+        </Text>
+      </ImageBackground>
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View
+            style={styles.modalView}
+            className="flex flex-col gap-y-5 bg-w1 dark:bg-bl h-fit justify-center align-center p-5"
+          >
+            <Text className="text-center text-xl font-bold text-bl dark:text-gr">
+              Ready to start your workout?{" "}
+            </Text>
+            <View className="flex flex-row gap-x-5 justify-center">
+              <TouchableOpacity
+                className="bg-red p-3 rounded-3xl w-[40%] flex justify-center"
+                onPress={() => setModalVisible(!modalVisible)}
+                activeOpacity={0.7}
+              >
+                <Text className="text-center text-w1 text-lg">No</Text>
+              </TouchableOpacity>
+              <Link
+                href={{
+                  pathname: "/session",
+                  params: { workoutName: item.name },
+                }}
+                asChild
+              >
+                <TouchableOpacity
+                  className="bg-gr p-3 rounded-3xl w-[40%] flex justify-center"
+                  onPress={() => setModalVisible(!modalVisible)}
+                  activeOpacity={0.7}
+                >
+                  <Text className="text-center text-w1 text-lg">Start</Text>
+                </TouchableOpacity>
+              </Link>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </TouchableOpacity>
   );
 };
 
 export default function Workouts() {
+  // const startWorkout = ({ item }: { item: ItemData }) => {
+  //   return (
+
+  //   );
+  // };
   const renderItem = ({ item }: { item: ItemData }) => {
     return <WorkoutActivity item={item} />;
   };
@@ -89,3 +139,42 @@ export default function Workouts() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+  },
+  modalView: {
+    borderRadius: 20,
+    padding: 5,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    width: "75%",
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    width: "30%",
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
